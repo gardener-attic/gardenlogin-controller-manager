6a2efbf0903d783caec9838c98d5d2bd1b48fda7
Peter Sutter <peter.sutter@sap.com>
2021-07-23 18:56:47 +0200

    write tls cert to contents dir

--- .landscaper/container/pkg/gardenlogin/operation_reconcile.go
+++ .landscaper/container/pkg/gardenlogin/operation_reconcile.go
@@ -8,15 +8,36 @@ package gardenlogin
 import (
 	"context"
 	"fmt"
-	"os"
-
-	"github.com/gardener/gardenlogin-controller-manager/.landscaper/container/pkg/api"
-
 	secretsutil "github.com/gardener/gardener/pkg/utils/secrets"
+	"github.com/gardener/gardenlogin-controller-manager/.landscaper/container/pkg/api"
+	"io/ioutil"
+	"os"
 )
 
 // Reconcile runs the reconcile operation.
 func (o *operation) Reconcile(ctx context.Context) (*api.Exports, error) {
+	tlsCert, err := o.loadOrGenerateGardenloginTlsCertificate()
+	if err != nil {
+		return nil, fmt.Errorf("could not load or generate gardenlogin tls certificate: %w", err)
+	}
+
+	err = ioutil.WriteFile(o.contents.GardenloginTlsKeyPemFile, tlsCert.PrivateKeyPEM, 0600)
+	if err != nil {
+		fmt.Errorf("failed to write tls key pem file to path %s: %w", o.contents.GardenloginTlsKeyPemFile, err)
+	}
+
+	err = ioutil.WriteFile(o.contents.GardenloginTlsPemFile, tlsCert.CertificatePEM, 0600)
+	if err != nil {
+		fmt.Errorf("failed to write tls pem file to path %s: %w", o.contents.GardenloginTlsPemFile, err)
+	}
+
+	return &o.exports, nil
+}
+
+// loadOrGenerateGardenloginTlsCertificate loads or generates the gardenlogin tls certificate.
+// It tries to restore the ca and tls certificate from the state
+// or generates new in case they are not valid or not within the validity threshold
+func (o *operation) loadOrGenerateGardenloginTlsCertificate() (*secretsutil.Certificate, error) {
 	caCertConfig := &secretsutil.CertificateSecretConfig{
 		CertType:   secretsutil.CACert,
 		CommonName: Prefix + ":ca",
@@ -53,13 +74,15 @@ func (o *operation) Reconcile(ctx context.Context) (*api.Exports, error) {
 		},
 	}
 
-	// TODO certResult
-	_, err = loadOrGenerateCertificate(o.state.GardenloginTlsKeyPemPath, o.state.GardenloginTlsKeyPemPath, certConfig, o.clock)
+	certResult, err := loadOrGenerateCertificate(o.state.GardenloginTlsKeyPemPath, o.state.GardenloginTlsKeyPemPath, certConfig, o.clock)
 	if err != nil {
 		return nil, fmt.Errorf("failed to load or generate certificate for webhook service: %w", err)
 	}
 
-	//o.contents.GardenloginTlsKeyPemFile
+	cert := certResult.certificate
+	if cert == nil {
+		return nil, fmt.Errorf("no certificate returned")
+	}
 
-	return &o.exports, nil
+	return cert, nil
 }
